a60bedb4c7880fa47322dc6b86533bf04668f075
Your Name <you@example.com>
2017-12-23 16:32:54 +0100

    add files to project

--- program.js
+++ program.js
@@ -3,14 +3,17 @@ process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
-        if (instruction === '/exit') {
-            process.stdout.write('Quitting app!\n');
-            process.exit();
-        } else if (instruction === '/info') {
-            process.stdout.write('Language: '+ process.env.lang);
-            process.stdout.write(', Version: ' + process.versions.node);  
-        } else {
-        process.stderr.write('Wrong instruction!\n');
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/info':
+                process.stdout.write('Language: '+ process.env.lang);
+                process.stdout.write(', Version: ' + process.versions.node); 
+                break;
+            default:  
+                process.stderr.write('Wrong instruction!\n');
         }
-}
+    }
 })
